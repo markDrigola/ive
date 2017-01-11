1030847952d455779f71ede9516d607b10c43003
Max <flexagon89@gmail.com>
2017-01-11 17:33:01 +0200

    pravki1

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
     $(document).on('ready', function () {
-        $('.all-input-number').mask("+99 (999) 999-999-99");
+        $('.all-input-number').mask("+9 999 999-999-99");
         $('.modal-class').on('click', function () {
             var imgSrc = $(this).children('img').attr('src');
             $('#myModal').find('img').attr('src',imgSrc );
